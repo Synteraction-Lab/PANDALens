b929d8685482db24cf6d3a71f0eaa12375c2caff
RenzoTsai <runze.cai@u.nus.edu>
2023-07-06 12:57:21 +0800

    select format

--- src/Command/SentGPTRequestCommand.py
+++ src/Command/SentGPTRequestCommand.py
@@ -1,5 +1,6 @@
 from src.Command.Command import Command
 from src.Utilities.json import detect_json
+import json
 
 
 class SendGPTRequestCommand(Command):
@@ -13,6 +14,7 @@ class SendGPTRequestCommand(Command):
         response = gpt.process_prompt_and_get_gpt_response(command=str(user_request))
 
         json_response = detect_json(response)
+        json_dict = json.loads(response)
         text_response = response
         audio_response = response
 
@@ -33,13 +35,31 @@ class SendGPTRequestCommand(Command):
 
                     audio_response = f"May I ask:\n {json_response['response']['question to users']}"
                     if audio_response.strip() == "May I ask:\n None":
-                        audio_response = "I have no question for you. Anything you want to add?"
+                        audio_response = "I have no questions for you. Anything you want to add?"
                     text_response = audio_response
 
                     self.system_config.audio_feedback_to_show = audio_response
                     self.system_config.notification = {'notif_type': 'text',
                                                        'content': f"{text_response}",
                                                        'position': 'middle-right'}
+                    
+                elif json_response['mode'] == "selecting":
+                    text_response = f"Moments:\n {json_response['response']}"
+                    # self.system_config.notification = {'notif_type': 'text',
+                    #                                    'content': f"{text_response}\n",
+                    #                                    'position': 'middle-right'}
+                    
+                    # for i in range(len(json_response['response'])):
+                    #     text_response += f"{i+1}: {json_response['response'][str(i)]}\n"
+                        
+               
+                    audio_response = "Here are the summaries of your moments."
+                    # self.system_config.text_feedback_to_show = text_response
+                    # self.system_config.audio_feedback_to_show = audio_response
+
+                    
+
+
 
         except Exception as e:
             print(e)
